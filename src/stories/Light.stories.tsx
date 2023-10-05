d1735ba60e7c243a22d20acc2ce2474fac3c65af
pskaro <wolfwar@outlook.com>
2023-10-05 17:49:31 +0200

    control and docs

--- src/stories/Light.stories.tsx
+++ src/stories/Light.stories.tsx
@@ -5,13 +5,24 @@ import Light from './Light';
 const meta: Meta<typeof Light> = {
   component: Light,
   title: 'Light',
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['green', 'yellow', 'red'],
+    },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {};
+export const Base: Story = {
+  args: {
+    variant: 'green',
+  },
+};
 
 export const Yellow: Story = {
   args: {
@@ -26,9 +37,19 @@ export const Red: Story = {
 };
 
 // custom render
-export const Another: Story = {
+export const ARobot: Story = {
   render: (args) => (
-    <div>
+    <div
+      style={{
+        background: 'gray',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10,
+        padding: 10,
+        border: '1px solid black',
+        width: 'max-content',
+      }}
+    >
       <Light variant='red' />
       <Light variant='yellow' />
       <Light variant='green' />
